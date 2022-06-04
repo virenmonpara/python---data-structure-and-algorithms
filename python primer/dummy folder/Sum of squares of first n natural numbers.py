b18b0c4e9047f86e984c7e898e52728686e87dee
virenmonpara <virenmonpara84@gmail.com>
2022-06-04 21:38:18 +0200

    learning python

--- python primer/dummy folder/Sum of squares of first n natural numbers.py
+++ python primer/dummy folder/Sum of squares of first n natural numbers.py
@@ -2,14 +2,14 @@
 
 def square(n):
     # long code
-    sum=0
-    for i in range(1,n,1):
-        sum=sum+i*i
+    sum = 0
+    for i in range(1, n, 1):
+        sum = sum+i*i
         return print(sum)
-    
+
     # short code
-    return print(sum(i**2 for i in range(1,n+1)))
+    return print(sum(i**2 for i in range(1, n+1)))
 
-x=int(input("Enter the number: "))
+
+x = int(input("Enter the number: "))
 square(x)
-

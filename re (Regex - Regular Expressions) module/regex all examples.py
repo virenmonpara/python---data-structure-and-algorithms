0c10a58084e0bc2748a52a2e2aeb0eb3f169e9be
virenmonpara <virenmonpara84@gmail.com>
2022-06-08 23:33:16 +0200

    re (Regex - Regular Expressions) module

--- re (Regex - Regular Expressions) module/regex all examples.py
+++ re (Regex - Regular Expressions) module/regex all examples.py
@@ -1,5 +1,5 @@
 import re
-# regex example learned from youtube link below
+# regex example i have learned from youtube link below
 # https://youtu.be/K8L6KVGG-7o
 text_to_search = ''' 
 abcdefghijklmnopqurtuvwxyz
@@ -15,11 +15,14 @@ coreyms.com
 
 321-555-4321
 123.555.1234
+354*564*6843
+800-595-4321
+900-655-4321
 
-Mr.Viren
-Mr.Urvish
-Mr.Ruchit
-Mr.Hasmukh
+Mr. Viren
+Mr Smith
+Ms Davis
+Mrs. Robinson0945
 Mr. T
 
 cat
@@ -27,28 +30,79 @@ pat
 bat
 mat
 '''
-sentence = 'Start a sentence and then bring it to an end'
+sentence = 'Hello Mr. Viren, how are you'
 
 
-# print('\t Tab')   # \t is tab
-# print(r'\t Tab')  # Raw String
+# print('\t viren')  #print viren after tab
+# print(r'\t viren')  # with raw string it will print \t viren
 # pattern=re.compile(r'\.') # r'\.' is used to escape the dot
-# pattern=re.compile(r'\d') # \d is digit
+# pattern=re.compile(r'\d') # \d for finding a digit in data
 # pattern=re.compile(r'[^b]at') # ^ is used to exclude the character
 # pattern = re.compile(r'\bHa') # \b is word boundary
-# pattern = re.compile(r'^Start')  # ^ is used to find at beginning of the string
-# pattern = re.compile(r'end$') # $ is used to find at end of the string
+# pattern = re.compile(r'^Hello')  # ^ is used to find at beginning of the string
+# pattern = re.compile(r'you$') # $ is used to find at end of the string
 # pattern=re.compile(r'\d\d\d') # to find three digit number in row
-# to find mentioned(number of \d) digit number in row
-pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+# pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d') # to find mentioned(number of \d) digit number in row
+# pattern = re.compile(r'\d{3}.\d{3}.\d{4}') #above mentioned pattern is also do the same
+# pattern = re.compile(r'\d\d\d[.-]\d\d\d[.-]\d\d\d\d') #finding numbers with .or- in row it will not show with *
+# pattern = re.compile(r'[89]00[.-]\d\d\d[.-]\d\d\d\d') #finding number starting with 800 or 900 and then followed by .or-
+# pattern=re.compile(r'[a-zA-Z0-9]') # to find all the characters in row
+# pattern=re.compile(r'Mr\.?\s[A-Z]\w*') # matching Mr. or Mr and then followed by capital letter and then followed by any character
+# pattern=re.compile(r'(Mr|Ms|Mrs)\.?\s[A-Z]\w+') # matching Mr. or Ms or Mrs and then followed by capital letter and then followed by 1 or more characters here Mr. T will excluded
+# matching Mr. or Ms or Mrs and then followed by capital letter and then followed by 0 or more characters here Mr. T will included
+pattern = re.compile(r'(Mr|Ms|Mrs)\.?\s[A-Z]\w*')
 
 # matches = pattern.finditer(sentence)
-# matches = pattern.finditer(text_to_search)
+matches = pattern.finditer(text_to_search)
 
-# for match in matches:
-#     print(match)
-with open('re (Regex - Regular Expressions) module\data.txt','r',encoding='utf-8') as f:
+for match in matches:
+    print(match)
+
+###############################################
+
+
+# pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+# pattern = re.compile(r'[89]00[.-]\d\d\d[.-]\d\d\d\d') #finding number starting with 800 or 900 and then followed by .or-
+pattern = re.compile(r'[a-z]')
+
+with open('data.txt', 'r', encoding='utf-8') as f:
     contents = f.read()
     matches = pattern.finditer(contents)
     for match in matches:
         print(match)
+
+####################################################
+
+# matching all email address
+pattern = re.compile(r'[a-zA-Z0-9_.-+]+@[a-zA-Z0-9-]+\.(com|edu|net)')
+# pattern = re.compile(r'[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]{2,3}') # to match first email address
+
+with open('emails.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+    matches = pattern.finditer(contents)
+    for match in matches:
+        print(match)
+
+#################################
+
+pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')  # matching all urls
+
+
+with open('urls.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+    matches = pattern.finditer(contents)
+    for match in matches:
+        # print(match)
+        print(match.group(1))  # we can use group(1) to get first group
+
+######################################
+
+pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')  # matching all urls
+
+
+with open('urls.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+    matches = pattern.finditer(contents)
+    for match in matches:
+        # print(match)
+        print(match.group(1))  # we can use group(1) to get first group

14afea9b68e2bc68dbb89745dec6669cd262d4e4
virenmonpara <virenmonpara84@gmail.com>
2022-06-10 03:45:36 +0200

    dictionary program

--- python primer/dictionary with keys having multiple inputs.py
+++ python primer/dictionary with keys having multiple inputs.py
@@ -16,3 +16,28 @@ dict[x, y, z] = x + y - z
 
 # print the dictionary
 print(dict)
+
+
+# another example
+
+# dictionary containing longitude and latitude of places
+places = {("19.07'53.2", "72.54'51.0"): "Mumbai",
+          ("28.33'34.1", "77.06'16.6"): "Delhi"}
+
+print(places)
+print('\n')
+
+# Traversing dictionary with multi-keys and creating
+# different lists from it
+lat = []
+long = []
+plc = []
+
+for i in places.keys():
+    lat.append(i[0])
+    long.append(i[1])
+    plc.append(places[i])
+
+print("Latitude:", lat)
+print("Longitude:", long)
+print("Places:", plc)

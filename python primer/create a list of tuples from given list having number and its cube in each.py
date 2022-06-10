7bec2aee4fcf1ae903eb4ea4417c38d2897bed08
virenmonpara <virenmonpara84@gmail.com>
2022-06-10 22:01:54 +0200

    dictionary program

--- python primer/create a list of tuples from given list having number and its cube in each.py
+++ python primer/create a list of tuples from given list having number and its cube in each.py
@@ -1,6 +1,7 @@
 def cube(lst):
-    return print([(x,x**3) for x in lst])
+    return print([(x, x**3) for x in lst])
 
-#driver code
-lst=[1,2,3,5,6]
+
+# driver code
+lst = [1, 2, 3, 5, 6]
 cube(lst)

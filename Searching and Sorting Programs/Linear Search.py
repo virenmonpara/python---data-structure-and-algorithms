a87a33424df9e73d01fbfc79b45fe5e4bbab697f
virenmonpara <virenmonpara84@gmail.com>
2022-06-11 02:14:55 +0200

    search program

--- Searching and Sorting Programs/Linear Search.py
+++ Searching and Sorting Programs/Linear Search.py
@@ -13,3 +13,8 @@ if linear_search(list, n):
     print("your number's position is: ", pos)
 else:
     print('number is not found in list')
+
+
+# another simple way
+if n in list:
+    print(list.index(n)+1)

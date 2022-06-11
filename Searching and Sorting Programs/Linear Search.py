6c6c5b6f853ffaf2c9a9f0f684ae634403ddc001
virenmonpara <virenmonpara84@gmail.com>
2022-06-11 02:01:48 +0200

    sort program

---
+++ Searching and Sorting Programs/Linear Search.py
@@ -0,0 +1,15 @@
+def linear_search(list, n):
+    for i in range(len(list)):
+        if list[i] == n:
+            # by defining global we make it global variale so we don't need to return pos
+            globals()['pos'] = i+1
+            return True
+
+
+# driver code
+list = [1, 2, 68, 125, 2548, 12647, 231696645]
+n = 125
+if linear_search(list, n):
+    print("your number's position is: ", pos)
+else:
+    print('number is not found in list')

077cc37220a2739fea4d5f334698503e8441d1fd
virenmonpara <virenmonpara84@gmail.com>
2022-07-09 20:18:22 +0200

    basic program

---
+++ python primer/print fibonacci series.py
@@ -0,0 +1,15 @@
+# print fibonacci series of n numbers = [1,1,2,3,5,8,13,21,......]
+
+def fibonacci(i):
+    lst = [1]
+    i0 = 0
+    i1 = 1
+    for x in range(0, i-1):
+        i2 = i0+i1
+        lst.append(i2)
+        i0 = i1
+        i1 = i2
+    return print(lst)
+
+
+fibonacci(10)

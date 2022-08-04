db73f9883087a5df305c4b4ce9e1259cca4a9a8e
virenmonpara <virenmonpara84@gmail.comn>
2022-08-04 13:29:48 +0200

    error handling

--- Defensive programming/Assertion.py
+++ Defensive programming/Assertion.py
@@ -1,9 +1,10 @@
-numbers = [1.5, 2.3, 0.7, -5, 4.4]
+numbers = [1.5, 2.3, 0.7, 0.2, 4.4]
 total = 0.0
 for num in numbers:
     assert num > 0.0, "numbers must be positive"
     total += num
-print('total is:', total) #here assertion throw an error because of negative number
+# here assertion throw an error because of negative number
+print('total is:', total)
 
 """The purpose of assertions is:
 • Help to ensure that no new bugs are introduced while adding
@@ -14,4 +15,3 @@ features or fixing other bugs.
 # x="hello"
 # assert x== "hello" # nothing happens
 # assert x== "goodbye" #AssertionError is raised
-
--- Defensive programming/Exception chaining.py
+++ Defensive programming/Exception chaining.py
@@ -1,12 +1,11 @@
-"""try:
-    age= int(input("enter your age in years: "))
-except ValueError as e:
-    print(f"unable to process input")
-    raise"""
+# try:
+#     age = int(input("enter your age in years: "))
+# except ValueError as e:
+#     print(f"error message: {e}")
+#     raise # raise the error again and take you to the error
 
-#
-try:
-    age= int(input("enter your age in years: "))
-except ValueError as e:
-    raise RuntimeError("unable to process input") from e
 
+# try:
+#     age= int(input("enter your age in years: "))
+# except ValueError as e:
+#     raise RuntimeError("unable to process input") from e
